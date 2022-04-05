e406a3f51e11a13bb00d3703fe1c5da8efc88efb
Lukas <lukas.randrianarisoa1@gmail.com>
2022-04-05 20:43:27 +0300

    Update db.config.js

--- m1p9mean-Lukas-Back/app/config/db.config.js
+++ m1p9mean-Lukas-Back/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/ekaly_db"
+    // url: "mongodb://localhost:27017/ekaly_db"
+    url: "mongodb+srv://mongodb-Lukas:<password>@cluster1.urqv1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   };
