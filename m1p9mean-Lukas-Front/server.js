3b7981275fc296904a96faf767a6dbffe72ad17b
Lukas Andrianina <lukas.randrianarisoa1@gmail.com>
2022-04-09 15:18:53 +0300

    Updates

--- m1p9mean-Lukas-Front/server.js
+++ m1p9mean-Lukas-Front/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/m1p9mean-lukas-front'));
+app.use(express.static(__dirname + '/dist/m1p9mean-Lukas-Front'));
 
-app.get('/', function(req,res) {
+app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/m1p9mean-lukas-front/index.html'));
+res.sendFile(path.join(__dirname+'/dist/m1p9mean-Lukas-Front/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
